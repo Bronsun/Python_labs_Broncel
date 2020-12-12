119ea1150b8a1205f568a8e7191738845e7e1810
Bronsun <mateusz.broncel1998@gmail.com>
2020-12-12 17:30:02 +0100

    Added Filozofowie.py

--- Multithreading/Filozofowie.py
+++ Multithreading/Filozofowie.py
@@ -2,57 +2,56 @@ import threading
 import random
 import time
 
-withDeadlock = False
+Deadlock = False
 
 
 class Philosopher(threading.Thread):
-    def __init__(self, name, forkOnLeft, forkOnRight):
+    def __init__(self,name,Left,Right):
         threading.Thread.__init__(self)
         self.name = name
-        self.forkOnLeft = forkOnLeft
-        self.forkOnRight = forkOnRight
+        self.Left = Left
+        self.Right = Right
 
     def run(self):
         while True:
-            print('{0} is thinking.'.format(self.name))
-            time.sleep(random.uniform(3, 13))
-            print('{0} is hungry.'.format(self.name))
-            self.tryGetForks()
-
-    def tryGetForks(self):
-        fork1, fork2 = self.forkOnLeft, self.forkOnRight
+            print('{0} myśli '.format(self.name))
+            time.sleep(random.uniform(3,13))
+            print('{0} jest głodny '.format(self.name) )
+            self.getForks()
+    def getForks(self):
+        fork_1,fork_2 = self.Left,self.Right
 
         while True:
-            fork1.acquire(True)
-            locked = fork2.acquire(False)
+            fork_1.acquire(True)
+            locked = fork_2.acquire(False)
             if locked:
                 break
-            if withDeadlock is False:
-                fork1.release()
-                print('{0} changes priority '.format(self.name))
-                fork1, fork2 = fork2, fork1
+            if Deadlock is False:
+                fork_1.release()
+                print('{0} zmienia swoje zaintersowanie '.format(self.name))
+                fork_1,fork_2 = fork_2,fork_1
             else:
-                print('{0} waiting for fork '.format(self.name))
+                print('{0} czeka na widelec '.format(self.name))
 
-        self.eating()
-        fork2.release()
-        fork1.release()
+            self.eating()
+            fork_2.release()
+            fork_1.release()
 
     def eating(self):
-        print('{0} starts eating'.format(self.name))
-        time.sleep(random.uniform(1, 10))
-        print('{0} finishes eating and leaves to think'.format(self.name))
+        print('{0} zaczyna jeść '.format(self.name))
+        time.sleep(random.uniform(1,10))
+        print('{0} kończy jedzenie i idzie myśleć dalej '.format(self.name))
 
 
-def testDiner():
+def diner():
     forks = [threading.Lock() for n in range(5)]
     names = ('Mati', 'Karol', 'Kacper', 'Michał', 'Zuza')
     philosophers = [Philosopher(names[i], forks[i % 5], forks[(i + 1) % 5]) for i in range(5)]
 
-    random.seed(123456)
+    random.seed(345662)
     for p in philosophers:
         p.start()
 
 
-if __name__ == "__main__":
-    testDiner()
+
+diner()
--- Multithreading/Histogram.py
+++ Multithreading/Histogram.py
@@ -1,29 +1,50 @@
+'''
 from multiprocessing.pool import Pool
-from Python.Algorithm.Matrix import Matrix
+import numpy as np
 import matplotlib.pyplot as plt
-import random as rn
+import random 
 
+def matrix(size):
+    macierz = np.random.rand(size,size)
+    return np.linalg.det(macierz) 
 
-def doSomeCalculation(size):
-    print("Started on matrix in size: ", size)
-    m1 = Matrix([[rn.randint(1, 10) for i in range(size)] for i in range(size)])
-    return m1.calcDeterminant() % 100000
 
 
 def testMultiThreading():
-    p = Pool(processes=4)
+    p = Pool(processes=2)
 
     sizes = [2, 4, 6, 8, 10, 12, 14]
 
-    result = p.map(doSomeCalculation, sizes)
+    result = p.map(matrix, sizes)
     p.close()
     p.join()
 
     plt.hist(result, bins=16)
     plt.show()
 
-    print('Result: \n {0}'.format(result))
 
 
 if __name__ == "__main__":
-    testMultiThreading()
+    print("XD")
+'''
+
+import multiprocessing
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    pool = multiprocessing.Pool()
+    num_figs = 20
+    input = zip(np.random.randint(10,1000,num_figs), 
+                range(num_figs))
+    pool.map(plot, input)
+
+def plot(args):
+    num, i = args
+    fig = plt.figure()
+    data = np.random.randn(num).cumsum()
+    plt.plot(data)
+    plt.title('Plot of a %i-element brownian noise sequence' % num)
+    fig.savefig('temp_fig_%02i.png' % i)
+
+main()
