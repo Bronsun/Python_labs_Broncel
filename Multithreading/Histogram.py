e28383b38e53a08834db15d9e7afb601f5db271e
Bronsun <mateusz.broncel1998@gmail.com>
2020-12-15 12:25:10 +0100

    Histogram

--- Multithreading/Histogram.py
+++ Multithreading/Histogram.py
@@ -1,4 +1,4 @@
-'''
+
 from multiprocessing.pool import Pool
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,45 +6,23 @@ import random
 
 def matrix(size):
     macierz = np.random.rand(size,size)
-    return np.linalg.det(macierz) 
+    x = np.linalg.det(macierz)
+    return x
 
 
 
 def testMultiThreading():
-    p = Pool(processes=2)
+    p = Pool(processes=4)
 
-    sizes = [2, 4, 6, 8, 10, 12, 14]
+    sizes = [2, 4, 6, 8, 10]
 
     result = p.map(matrix, sizes)
     p.close()
     p.join()
-
-    plt.hist(result, bins=16)
+    
+    plt.hist(result, bins=10)
     plt.show()
 
-
-
 if __name__ == "__main__":
-    print("XD")
-'''
-
-import multiprocessing
-import matplotlib.pyplot as plt
-import numpy as np
-
-def main():
-    pool = multiprocessing.Pool()
-    num_figs = 20
-    input = zip(np.random.randint(10,1000,num_figs), 
-                range(num_figs))
-    pool.map(plot, input)
-
-def plot(args):
-    num, i = args
-    fig = plt.figure()
-    data = np.random.randn(num).cumsum()
-    plt.plot(data)
-    plt.title('Plot of a %i-element brownian noise sequence' % num)
-    fig.savefig('temp_fig_%02i.png' % i)
-
-main()
+    
+    testMultiThreading()
